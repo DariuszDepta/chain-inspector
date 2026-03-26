8ac750ae2d5626a7517806a7e140b25bac4b7ee8
Dariusz Depta <141360751+DariuszDepta@users.noreply.github.com>
2026-03-26 16:19:05 +0100

    Updates.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
+use base64::Engine;
+use base64::engine::general_purpose;
+
 const URL: &str = "https://rpc-lb.neutron.org:443";
+
+const SIZE_THRESHOLD: usize = 256 * 1024; // 256kB
 
 /// Gets the height of the latest block.
 ///
@@ -40,12 +45,12 @@ async fn get_transaction_hashes(url: &str, height: u64) -> anyhow::Result<Vec<St
 /// curl -s "https://rpc-lb.neutron.org:443/tx?hash=0xD542E9FC635E8D224A54B5A668BE7A3ED4FC6F09FA48F9E0ED1A5D9616295419" \
 /// | jq -r '[.result.tx_result.events[] | select(.type == "message") | .attributes[] | select(.key == "action") | .value | select(startswith("/"))]'
 /// ```
-async fn get_msg_types(url: &str, hash: &str) -> anyhow::Result<Vec<String>> {
+async fn get_msg_types(url: &str, hash: &str) -> anyhow::Result<(Vec<String>, usize)> {
     let mut msg_types = vec![];
     let command = format!("{}/tx?hash=0x{}", url, hash);
     let response: serde_json::Value = reqwest::get(command).await?.json().await?;
     let Some(events) = &response["result"]["tx_result"]["events"].as_array() else {
-        return Ok(msg_types);
+        return Ok((msg_types, 0));
     };
     for event in events.iter() {
         let event_type = event["type"].as_str().unwrap();
@@ -64,27 +69,45 @@ async fn get_msg_types(url: &str, hash: &str) -> anyhow::Result<Vec<String>> {
             }
         }
     }
-    Ok(msg_types)
+    if !msg_types.is_empty() {
+        let tx = &response["result"]["tx"].as_str().unwrap().to_string();
+        let bytes: Vec<u8> = general_purpose::STANDARD
+            .decode(tx)
+            .expect("invalid base64");
+        let length = bytes.len();
+        Ok((msg_types, length))
+    } else {
+        Ok((msg_types, 0))
+    }
 }
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let mut height = get_latest_block_height(URL).await?;
     println!("last block height = {}", height);
-    let mut count = 100;
-    while count > 0 {
+    let mut max_length = 0;
+    while height > 0 {
         let hashes = get_transaction_hashes(URL, height).await?;
         if !hashes.is_empty() {
-            println!("height = {}\n  {:?}", height, hashes);
+            println!("height = {}", height);
             for hash in &hashes {
-                let msg_types = get_msg_types(URL, hash).await?;
-                for msg_type in msg_types {
-                    println!("{}", msg_type);
+                let (msg_types, length) = get_msg_types(URL, hash).await?;
+                if !msg_types.is_empty() {
+                    for msg_type in &msg_types {
+                        println!("  {}", msg_type);
+                    }
+                    if length > max_length {
+                        max_length = length;
+                    }
+                    println!("> {} | {}", max_length, length);
+                    if max_length >= SIZE_THRESHOLD {
+                        println!("SIZE THRESHOLD EXCEEDED!\nhash={}\nheight={}", hash, height);
+                        std::process::exit(1);
+                    }
                 }
             }
         }
         height -= 1;
-        count -= 1;
     }
     Ok(())
 }
